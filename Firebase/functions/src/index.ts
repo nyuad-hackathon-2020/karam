650f797c05971bd1180f09bab4d1ea37f84e6daf
Phillip Wong <hncswpy@gmail.com>
2020-04-18 00:40:52 +0400

    queueing cloud function updates

--- Firebase/functions/src/index.ts
+++ Firebase/functions/src/index.ts
@@ -4,11 +4,24 @@ import * as admin from 'firebase-admin';
 "use strict"
 
 export const order_queue = functions.https.onRequest((request, response) => {
-    const sender = request.get('sender')
-    const seller = request.get('seller')
-    const product = request.get('content')
+   
+    const sender = request.get('sender')!
+    const seller = request.get('seller')!
+    const product = request.get('content')!
     const timeReceived = Date()
-    admin.firestore().doc('restaurants/{restaurantsID}/orders/{orderID}')
-    .create()
-});
+    const db = admin.firestore()
+    db.collection('restaurants').doc(seller).collection('orders')
+    .add({
+        'by' : sender,
+        'at' : timeReceived,
+        'content': product
+    }).then(function(docRef){
+        console.log("Document order created with ID:",docRef.id)
+        response.send('Success')
+    })
+    .catch(function(error){
+        console.error("Failed to add document order",error);
+        response.send(JSON.stringify(error))
+    });
+})
  
